29105e4733d374be5adae71acf659abf57b01f6d
MaartenThijs <Maarten.Thijs@wisemen.digital>
2024-10-02 09:08:21 +0200

    Updated carousel to accept providers or urls

--- lib/widgets/custom_image_carousel.dart
+++ lib/widgets/custom_image_carousel.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dismissible_page/dismissible_page.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -5,21 +6,26 @@ import 'package:photo_view/photo_view.dart';
 
 class CustomImageCarousel extends ConsumerWidget {
   const CustomImageCarousel({
-    required this.imageProviders,
     required this.controller,
     this.photoViewBackgroundColor,
     this.dismissThresholds,
     this.dismissDirection,
+    this.imageProviders,
     this.initialPage,
     this.errorWidget,
+    this.imageUrls,
     this.maxScale,
     this.heroTag,
     super.key,
-  });
+  }) : assert(
+          imageProviders != null || imageUrls != null,
+          'Either imageProviders or imageUrls must be provided',
+        );
 
   final Map<DismissiblePageDismissDirection, double>? dismissThresholds;
-  final List<ImageProvider<Object>> imageProviders;
+  final List<ImageProvider<Object>>? imageProviders;
   final PageController controller;
+  final List<String>? imageUrls;
   final Widget? errorWidget;
   final int? initialPage;
 
@@ -46,14 +52,14 @@ class CustomImageCarousel extends ConsumerWidget {
           },
       child: PageView.builder(
         controller: controller,
-        itemCount: imageProviders.length,
+        itemCount: (imageProviders ?? imageUrls)!.length,
         itemBuilder: (context, index) => PhotoView(
           backgroundDecoration: BoxDecoration(
             color: photoViewBackgroundColor ?? Colors.transparent,
           ),
           maxScale: PhotoViewComputedScale.covered * (maxScale ?? 2.0),
           heroAttributes: PhotoViewHeroAttributes(
-            tag: heroTag ?? imageProviders[index].hashCode,
+            tag: heroTag ?? (imageProviders ?? imageUrls)![index].hashCode,
           ),
           errorBuilder: (context, error, stackTrace) => GestureDetector(
             onTap: () => Navigator.of(context).pop(),
@@ -70,7 +76,10 @@ class CustomImageCarousel extends ConsumerWidget {
                   ),
                 ),
           ),
-          imageProvider: imageProviders[index],
+          imageProvider: imageProviders?[index] ??
+              CachedNetworkImageProvider(
+                imageUrls![index],
+              ),
         ),
       ),
     );
